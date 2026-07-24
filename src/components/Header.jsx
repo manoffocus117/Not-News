7a0cc3d2d2719987bbda23c1cf1df717275b06b9
MD Saifur Rahman <saifurrahmansaif954@gmail.com>
2026-07-25 04:03:25 +0600

    custom theme and dynamic time added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { format } from "date-fns";
 
 const Header = () => {
       return (
             <header className="py-5">
                   <div className="flex flex-col items-center justify-center gap-3">
-                        <img className="w-80" src={logo} alt="" srcset="" />
-                        <p className="text-xl text-gray-400">Journalism Without Fear or Favour</p>
+                        <img className="w-80" src={logo} alt="logo" />
+                        <p className="text-xl text-secondary">
+                              Journalism Without Fear or Favour
+                        </p>
+                        <p>
+                              <span className="font-semibold text-primary">
+                                    {format(new Date(), "EEEE, ")}
+                              </span>{" "}
+                              <span className="font-medium text-secondary">{format(new Date(), "MMMM L, yyyy")}</span>
+                        </p>
                   </div>
             </header>
       );
