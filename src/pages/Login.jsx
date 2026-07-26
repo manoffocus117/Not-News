820a73bcf3a489c9f86184831119ca4a54dd7048
MD Saifur Rahman <saifurrahmansaif954@gmail.com>
2026-07-27 00:55:24 +0600

    registation form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router";
 const Login = () => {
       return (
             <fieldset className="fieldset bg-white border-base-300 rounded-box w-2xl mx-auto border p-24 space-y-5">
-                  <h2 className="text-3xl font-semibold text-center mb-12 pb-12 border-b border-base-200">Login your account</h2>
+                  <h2 className="text-3xl font-semibold text-primary text-center mb-12 pb-12 border-b border-base-200">
+                        Login your account
+                  </h2>
                   <div className="space-y-3">
-                        <label className="label text-xl font-semibold">
+                        <label className="label text-xl font-semibold text-primary">
                               Email address
                         </label>
                         <input
@@ -17,7 +19,7 @@ const Login = () => {
                   </div>
 
                   <div className="space-y-3">
-                        <label className="label text-xl font-semibold">
+                        <label className="label text-xl font-semibold text-primary">
                               Password
                         </label>
                         <input
@@ -26,7 +28,7 @@ const Login = () => {
                               placeholder="Enter your password"
                         />
                   </div>
-                  <button className="btn btn-primary mt-4">Login</button>
+                  <button className="btn btn-primary text-white mt-4 p-7">Login</button>
                   <p className="text-[1rem]">
                         Don't Have An Account ?{" "}
                         <Link to={"/auth/register"} className="text-accent">
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-      return <div>Register</div>;
+      return (
+            <fieldset className="fieldset bg-white border-base-300 rounded-box w-2xl mx-auto border p-24 space-y-5">
+                  <h2 className="text-3xl text-primary font-semibold text-center mb-12 pb-12 border-b border-base-200">
+                        Register your account
+                  </h2>
+                  <div className="space-y-3">
+                        <label className="label text-xl font-semibold text-primary">
+                              Your Name
+                        </label>
+                        <input
+                              type="text"
+                              className="input w-full p-7 text-[1rem] border-0 outline-none"
+                              placeholder="Enter your name"
+                        />
+                  </div>
+                  <div className="space-y-3">
+                        <label className="label text-xl font-semibold text-primary">
+                              Photo URL
+                        </label>
+                        <input
+                              type="url"
+                              className="input w-full p-7 text-[1rem] border-0 outline-none"
+                              placeholder="Enter your Photo URL"
+                        />
+                  </div>
+                  <div className="space-y-3">
+                        <label className="label text-xl font-semibold text-primary">
+                              Email address
+                        </label>
+                        <input
+                              type="email"
+                              className="input w-full p-7 text-[1rem] border-0 outline-none"
+                              placeholder="Enter your email address"
+                        />
+                  </div>
+
+                  <div className="space-y-3">
+                        <label className="label text-xl font-semibold text-primary">
+                              Password
+                        </label>
+                        <input
+                              type="password"
+                              className="input w-full p-7 text-[1rem] border-0 outline-none"
+                              placeholder="Enter your password"
+                        />
+                  </div>
+                  <label className="label text-secondary text-[1rem]">
+                        <input
+                              type="checkbox"
+                              defaultChecked
+                              className="checkbox"
+                              required
+                        />
+                        Accept Term & Conditions
+                  </label>
+                  <button className="btn btn-primary text-white mt-4 p-7">
+                        Register
+                  </button>
+                  <p className="text-[1rem]">
+                        Already Have An Account ?{" "}
+                        <Link to={"/auth/login"} className="text-accent">
+                              Login
+                        </Link>
+                  </p>
+            </fieldset>
+      );
 };
 
 export default Register;
